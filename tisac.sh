58abb68edcb7abcf13f24a4f1ea4a93ea593b9ad
NocturnalRushers <122140360+NocturnalRushers@users.noreply.github.com>
2024-02-01 21:26:22 +0800

    Update tisac.sh

--- tisac.sh
+++ tisac.sh
@@ -61,7 +61,7 @@ git clone -b test https://github.com/teralomaniac/clewd
 
 echo -e "\033[0;33m本操作仅为破限下载提供方便，所有破限皆为收录，不具有破限所有权\033[0m"
 read -p "回车进行导入"
-git clone https://github.com/hopingmiao/promot.git st_promot
+git clone https://github.com/NocturnalRushers/promot.git st_promot
 if  [ ! -d "st_promot" ]; then
     echo -e "\n\033[0;33m hoping：因网络波动预设文件下载失败了，更换网络后再试\n\033[0m"
 else
